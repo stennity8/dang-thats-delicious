ce91017a55857d414c500569c59ddeb98a0921c4
Shawn <shawntennity@gmail.com>
2020-01-07 09:57:49 -0500

    Finished search interface.

--- public/javascripts/modules/typeAhead.js
+++ public/javascripts/modules/typeAhead.js
@@ -1,4 +1,5 @@
-const axios = require('axios');
+import axios from 'axios';
+import dompurify from 'dompurify';
 
 function searchResultsHTML(stores) {
   return stores
@@ -26,14 +27,20 @@ function typeAhead(search) {
 
     // show the search results
     searchResults.style.display = 'block';
-    searchResults.innerHTML = '';
 
     axios
       .get(`/api/search?q=${this.value}`)
       .then(res => {
         if (res.data.length) {
-          searchResults.innerHTML = searchResultsHTML(res.data);
+          searchResults.innerHTML = dompurify.sanitize(
+            searchResultsHTML(res.data)
+          );
+          return;
         }
+        // tell them notheing came back
+        searchResults.innerHTML = dompurify.sanitize(
+          `<div class="search__result">No results for ${this.value} found!</div>`
+        );
       })
       .catch(err => {
         console.error(err);
@@ -53,8 +60,18 @@ function typeAhead(search) {
     if (e.keyCode === 40 && current) {
       next = current.nextElementSibling || items[0];
     } else if (e.keyCode === 40) {
-      next === items[0];
+      next = items[0];
+    } else if (e.keyCode === 38 && current) {
+      next = current.previousElementSibling || items[items.length - 1];
+    } else if (e.keyCode === 38) {
+      next = items[items.length - 1];
+    } else if (e.keyCode === 13 && current.href) {
+      window.location = current.href;
     }
+    if (current) {
+      current.classList.remove(activeClass);
+    }
+    next.classList.add(activeClass);
   });
 }
 
