ade58ff10eba7a61fe2ff7e01441dd9d7ac78e23
Shawn <shawntennity@gmail.com>
2020-01-11 11:35:39 -0500

    Set up map and display of stores.

--- routes/index.js
+++ routes/index.js
@@ -53,9 +53,9 @@ router.post(
   authController.confirmedPasswords,
   catchErrors(authController.update)
 );
+router.get('/map', storeController.mapPage);
 
 // API
-
 router.get('/api/search', catchErrors(storeController.searchStores));
 
 router.get('/api/stores/near', catchErrors(storeController.mapStores));
